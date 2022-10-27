8198fd0b64038bd972c2cfb80516bc355ad76271
chan lap ping <chanlapping64@gmail.com>
2022-10-27 11:42:37 +0800

    Add functions for operator, equal and clear buttons.

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (b === 0) {
+        return 'Error! Division by 0.';
+    }
     return a / b;
 }
 
@@ -27,12 +30,75 @@ function operate(operator, a, b) {
     }
 }
 
+// initialize state of machine
 let displayValue = '';
+let storedValue = '';
+let operator = '';
+
+// UI components
 const display = document.querySelector('.display');
-
 const numBtns = document.querySelectorAll('.num');
+const operatorBtns = document.querySelectorAll('.operator');
+const equalBtn = document.querySelector('.operate');
+const clearBtn = document.querySelector('.clear');
 
+
+// event listeners
 numBtns.forEach(btn => btn.addEventListener('click', (e) => {
-    displayValue += e.target.innerHTML;
+    displayValue += e.target.value;
     display.textContent = displayValue;
 }));
+
+operatorBtns.forEach(btn => btn.addEventListener('click', (e) => {
+    if (displayValue === '') {
+        return;
+    }
+    if (storedValue === '') {
+        storedValue = +displayValue;
+    }
+    else {
+        storedValue = operate(operator, storedValue, +displayValue);
+        display.textContent = storedValue;
+    }
+    operator = e.target.value;
+    displayValue = '';
+}));
+
+equalBtn.addEventListener('click', () => {
+    if (storedValue === '' || displayValue === '') {
+        return;
+    }
+    display.textContent = operate(operator, storedValue, +displayValue);
+    storedValue = '';
+    displayValue = '';
+    operator = '';
+});
+
+clearBtn.addEventListener('click', () => {
+    displayValue = '';
+    storedValue = '';
+    operator = '';
+    display.textContent = '0';
+})
+
+// store the first number that is input into the calculator when a user presses an operator
+// save which operation has been chosen
+// operate() on them when the user presses the “=” key
+// once operate() has been called, update the display with the ‘solution’ to the operation
+// 
+// operator click
+// if first number = ''
+//   first number = display value
+//   operator = button value
+// else
+//   result = operate(operator, first, display value)
+//   first number = result
+//   display result
+// display value = ''
+
+// equal btn click
+// if first = '' or display value = '' return
+// result = operate(operator, first, display value)
+// display result
+// first = ''
+// display value = ''
